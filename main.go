10398f75e96242b61461f6a11556cb8e6d49ad9d
Wahyu Wardaya <147429375+WahyuWardaya@users.noreply.github.com>
2025-03-20 17:59:15 +0800

    Koneksi Database dengan GORM dan Membuat API dengan Gin

--- main.go
+++ main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+type User struct{
+	ID 			uint		`gorm:"column:id;primary_key"`
+	Name 		string		`gorm:"column:name"`
+	Email 		string		`gorm:"column:email"`
+	Age 		int			`gorm:"column:age"`
+	CreatedAt 	time.Time	`gorm:"column:created_at"`
+	UpdatedAt 	time.Time	`gorm:"column:updated_at"`
 }
 
+
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil)
+	dsn := "root:@tcp(localhost:3306)/openapi?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil{
+		fmt.Println("Tidak berhasil konek ke database")
+	}
+	router := gin.Default()
+	
+	router.GET("/users", func(c *gin.Context) {
+		var users []User
+		db.Find(&users)
+		c.JSON(http.StatusOK, gin.H{"data":users})
+	})
+
+		
+	router.Run(":3000")
 }
